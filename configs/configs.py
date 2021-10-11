f03bb41cdd01d99ba27eb9b9d039f45a6f1a54f2
Kaiyi <kaiyiling3@gmail.com>
2021-10-11 23:05:14 +0800

    first init

---
+++ configs/configs.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+pjt_config = {
+   "database": {
+        "auth" : "",
+        "db_name" : "",
+        "collection_name" : ""
+   },
+   "train": {
+       "loss" : tf.keras.losses.CategoricalCrossentropy(),
+       "metrics": tf.keras.metrics.CategoricalAccuracy()
+   },
+   "model": {
+       "input_fmri": (64, 64, 64, 1),
+       "input_diffusion": (128, 128, 128, 1)
+   }
+}
